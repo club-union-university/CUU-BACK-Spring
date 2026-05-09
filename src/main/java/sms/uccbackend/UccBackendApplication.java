1703d2de0de571e923845da97e8c3420d6306d20
tjfakstn <tjfakstn04@gmail.com>
2026-05-09 18:38:37 +0900

    [INIT]: Repository connect

---
+++ src/main/java/sms/uccbackend/UccBackendApplication.java
@@ -0,0 +1,13 @@
+package sms.uccbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UccBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UccBackendApplication.class, args);
+    }
+
+}
